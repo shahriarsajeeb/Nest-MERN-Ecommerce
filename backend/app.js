329197d2c128921391690fe2ee5679a5859574d0
shahriarsajeeb <sajeebshahriar665@gmail.com>
2022-05-01 04:21:43 -0700

    mobile-app-issues-fix-done

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const path = require("path");
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
-app.use(fileUpload());
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(fileUpload({useTempFiles: true}));
 
 // config
 if(process.env.NODE_ENV!=="PRODUCTION"){
