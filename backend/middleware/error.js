ad399e637b67a9f1969dd82930e235e4f75fb31c
shahriarsajeeb <sajeebshahriar665@gmail.com>
2022-05-01 07:39:28 -0700

    mobile-app-issues-fix-done

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,7 +2,7 @@ const ErrorHandler = require("../utils/ErrorHandler");
 
 module.exports = (err,req,res,next) =>{
     err.statusCode = err.statusCode || 500
-    err.message = err.message || "Interval error"
+    err.message = err.message || "Internal server error"
 
     // wrong mongodb id error
     if(err.name === "CastError"){
@@ -31,7 +31,7 @@ module.exports = (err,req,res,next) =>{
 
     res.status(err.statusCode).json({
         success: false,
-        message: err.stack
+        message: err.message
     })
 }
 
